7609d485849ff02c19035a801205814d1353d6de
Jackhax <83289186+Packjackisback@users.noreply.github.com>
2023-07-16 22:59:06 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import requests
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+@bot.command()
+async def speedrun(ctx, game):
+    api_url = f'https://www.speedrun.com/api/v1/games/{game}/records?top=1'
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data['data']:
+            run = data['data'][0]['runs'][0]['run']
+            player = run['players'][0]['id']
+            time = run['times']['primary']
+            await ctx.send(f'Current world record for {game}: {time} by {player}')
+        else:
+            await ctx.send('No records found for the specified game.')
+    else:
+        await ctx.send('Failed to fetch data from speedrun.com')
+
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot.run('YOUR_BOT_TOKEN')
